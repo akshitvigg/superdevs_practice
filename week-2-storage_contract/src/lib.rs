7dc0acf7e37565493f0bcbea4d69e402e9f788a5
akshitvigg <akshitvig48@gmail.com>
2025-09-03 02:31:53 +0530

    last checkpoint

--- week-2-storage_contract/src/lib.rs
+++ week-2-storage_contract/src/lib.rs
@@ -1,1 +1,19 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
+};
 
+entrypoint!(process_instruction);
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub enum NameInstruction {
+    Initialize(String),
+    Update(String),
+}
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+}
