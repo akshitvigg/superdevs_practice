596b2fcf6c17f70e042718e9b37edbea67ab8c27
akshitvigg <akshitvig48@gmail.com>
2025-09-05 15:14:17 +0530

    added counter and week 2 changes

---
+++ counter-anchor/programs/counter-anchor/src/lib.rs
@@ -0,0 +1,45 @@
+use anchor_lang::prelude::*;
+
+declare_id!("5Wnpeo6hA75WGacPT9THrf1GFYsUky5JnEfh2b91tgkc");
+
+#[program]
+pub mod counter_anchor {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let new_account = &mut ctx.accounts.new_account;
+        new_account.owner = ctx.accounts.signer.key();
+        new_account.count = 0;
+        Ok(())
+    }
+}
+
+#[account]
+pub struct AccountInstruction {
+    pub owner: Pubkey,
+    pub count: u64,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer=signer, space = 8+8)]
+    pub new_account: Account<'info, AccountInstruction>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, has_one = owner @ ErrorCode::Unauthorized)]
+    pub new_account: Account<'info, AccountInstruction>,
+
+    pub owner: Signer<'info>,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("you are not authorized to perform this action")]
+    Unauthorized,
+}
